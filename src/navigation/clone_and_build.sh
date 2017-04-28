dfd1270a06d16fe49714921b5faa27330efef362
asanoaozora <fifitaneki@hotmail.com>
2017-04-28 16:05:11 +0200

    update navit version and tested

--- src/navigation/clone_and_build.sh
+++ src/navigation/clone_and_build.sh
@@ -2,7 +2,7 @@
 
 build_option=""
 positioning_version='9725fe1f553197042d6445997690d452a73490c0'
-navit_version='995cec54c8682fbabfb4f912b6156ce0b5b43436'
+navit_version='658060b86596efb270cfcd21e118a9071f5d5092'
 
 echo "version of positioning is: $positioning_version"
 echo "version of navit is: $navit_version"
